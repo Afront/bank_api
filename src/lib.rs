6b9a87df144245969939b0d03de108cc268b6b96
Afront <3943720+Afront@users.noreply.github.com>
2020-01-09 12:18:21 +0900

    Added bank account

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+	use serde::{Deserialize, Serialize};
+//	use serde_json::Result;
+
+	#[derive(Debug, Deserialize, Serialize)]
+	struct BankAccount {
+		account_number: String,
+		balance: u64,
+		transaction_history: Vec<String>,
+	}
+
+	#[test]
+	fn bank_account_test() {
+		let account = BankAccount {account_number: "012-321".to_owned(), balance: 0, transaction_history: vec!["hi".to_owned()]};
+		assert_eq!(account.account_number , "012-321".to_owned());
+		assert_eq!(account.balance , 0);
+		assert_eq!(account.transaction_history[0] , "hi");
+	}
+
 }

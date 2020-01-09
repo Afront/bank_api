5300e6c01189c9c89cd9d082ae036f148b141f6a
Afront <3943720+Afront@users.noreply.github.com>
2020-01-09 12:33:04 +0900

    Fixed some formatting issues

--- src/lib.rs
+++ src/lib.rs
@@ -20,9 +20,9 @@ mod tests {
 	#[test]
 	fn bank_account_test() {
 		let account = BankAccount {account_number: "012-321".to_owned(), balance: 0, transaction_history: vec!["hi".to_owned()]};
-		assert_eq!(account.account_number , "012-321".to_owned());
-		assert_eq!(account.balance , 0);
-		assert_eq!(account.transaction_history[0] , "hi");
+		assert_eq!(account.account_number, "012-321".to_owned());
+		assert_eq!(account.balance, 0);
+		assert_eq!(account.transaction_history[0], "hi");
 	}
 
 	#[test]
